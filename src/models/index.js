6a842f24e7a7e2c7820272cd1aed5ba232bff8af
Ujjawal Shrivastava <shrivastava.uj@northeastern.edu>
2024-09-26 11:33:08 -0400

    Updated changes to index.js to sequelize the object

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,5 @@
+//create a sequelize object
+//Authenticate if the sequelize object is able to establish connection with the database
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
