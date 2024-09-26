b648436258a6e1028f20ef31432f1f7d059abc2f
Ujjawal Shrivastava <shrivastava.uj@northeastern.edu>
2024-09-26 11:30:22 -0400

    added .history to gitignore and made proper comments in health.js

--- src/routes/healthCheck.js
+++ src/routes/healthCheck.js
@@ -1,18 +1,20 @@
 //Health check route used to fetch the details from sequelize object and check the request
-//Error handling for any other request other than GET
-//Error handling for Database connection fail 503
+
+
 
 const express = require('express');
 const router = express.Router();
 const { sequelize } = require('../models');
 
 // Middleware to allow only GET requests
+//Error handling for any other request other than GET
 router.all('/', (req, res, next) => {
   if (req.method !== 'GET') {
       return res.status(405).json({ error: "Method Not Allowed" }).setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   }
   next();
 });
+
 
 router.get('/', async (req, res) => {
   try {
@@ -22,6 +24,7 @@ router.get('/', async (req, res) => {
   } catch (error) {
       console.error('Database connection failed:', error);
       // Only attempt to set headers and send a response if no response has been sent yet:
+      //Error handling for Database connection fail 503
       if (!res.headersSent) {
           res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
           res.status(503).send();
